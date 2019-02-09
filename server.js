b750ce1b59f4e7622855db230d0e893abd0b34f0
Marcel Thomas <marcelthomas@AND0373.local>
2019-02-09 12:57:29 +0000

    First commit

---
+++ server.js
@@ -0,0 +1,37 @@
+if (!process.env.DROPBOX_ACCESS_TOKEN) {
+  throw 'Missing "DROPBOX_ACCESS_TOKEN" environment variable'
+}
+const express = require('express')
+const path = require('path')
+const app = express()
+const PORT = process.env.PORT || 5000
+const Dropbox = require('dropbox-extra')
+const dropbox = new Dropbox(process.env.DROPBOX_ACCESS_TOKEN)
+const cmd = require('node-cmd')
+
+const syncDropbox = () => {
+  try {
+    cmd.get(
+      'rm -rf public-temp && mkdir public-temp', (err, data, stderr) => {
+        try {
+          dropbox.sync('my-app', 'public-temp', (err, cursor) => {
+            try {
+              cmd.run('rm -rf public && mv ./public-temp ./public')
+          
+              dropbox.sync(cursor)
+            } catch (error) {}
+          });
+        } catch (error) {}
+      }
+    );
+  } catch (error) {}
+}
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.get('/sync', (req, res) => {
+  syncDropbox()
+  res.json({
+    message: 'Sync started'
+  })
+})
+app.listen(PORT, () => console.log(`> Ready on http://localhost:${PORT}`))
